ed09a667f3940b5ff3cdc6d1bc701e50fc3f29c7
Aken Hu <aken.hu@silkrode.com.tw>
2021-01-15 04:56:15 +0000

    Resolve "feText 的 style 和 class 需支持 function"

--- src/components/Text/src/index.vue
+++ src/components/Text/src/index.vue
@@ -1,8 +1,11 @@
 <script>
-import { isFunction } from '@/utils/lodash';
+import {
+  isFunction, isObject, isArray, isString,
+} from '@/utils/lodash';
 
 export default {
   name: 'FeText',
+  inheritAttrs: false,
   model: { prop: 'value', event: 'change' },
   props: {
     value: {
@@ -12,6 +15,24 @@ export default {
     format: {
       type: Function,
       default: null,
+    },
+  },
+  computed: {
+    className() {
+      const defaultClass = 'fe-text';
+      const className = isFunction(this.$attrs.class) ? this.$attrs.class() : this.$attrs.class;
+
+      if (isArray(className)) {
+        return [defaultClass, ...className];
+      }
+      if (isObject(className)) {
+        return { [defaultClass]: true, ...className };
+      }
+      if (isString(className)) {
+        return `${defaultClass} ${className}`;
+      }
+
+      return defaultClass;
     },
   },
   methods: {
@@ -25,7 +46,8 @@ export default {
     return h(
       'span',
       {
-        class: 'fe-text',
+        class: this.className,
+        style: isFunction(this.$attrs.style) ? this.$attrs.style() : this.$attrs.style,
       },
       this.handleDisplay(),
     );
