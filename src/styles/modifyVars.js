0a11fc036395e4cd13a895a96c239acfc7a075b1
ray_tsai <ray.tsai@silkrode.com.tw>
2021-01-18 11:52:24 +0800

    [refactor] arrange less files, remove modifyVars(used global .less to rewrite)

--- src/styles/modifyVars.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'primary-color': '#fa6400',
-  // 'text-color': 'rgba(0, 0, 0, .65)',
-  // 'layout-header-background': '#dbb29e',
-  'layout-sider-background': 'white',
-  // 'layout-body-background': '#f5f5f5',
-};
