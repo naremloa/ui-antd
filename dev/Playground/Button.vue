be4c7c2ccd59e03fdef53ae9b0560df3be3f0a04
ray.tsai <ray.tsai@silkrode.com.tw>
2021-01-26 05:57:17 +0000

    [component] fe-add-button, add disabled

--- dev/Playground/Button.vue
+++ dev/Playground/Button.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    {{ $route.name }}
+    <fe-add-button
+      :disabled="isDisabled"
+      title="增加按鈕" />
+    <fe-add-button
+      title="增加按鈕 disabled" />
   </div>
 </template>
 
@@ -8,6 +12,16 @@
 
 export default {
   name: 'Button',
+  data() {
+    return {
+      isDisabled: false,
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.isDisabled = true;
+    }, 1500);
+  },
 };
 </script>
 
--- src/components/AddButton/src/index.vue
+++ src/components/AddButton/src/index.vue
@@ -1,25 +1,30 @@
 <template>
-  <p
+  <button
     class="add-button"
+    :class="disabled && 'add-button--disabled'"
+    :disabled="disabled"
     @click="handleAddButton">
     <span class="add-button__icon" />
     <span class="add-button__title">{{ title }}</span>
-  </p>
+  </button>
 </template>
 <script>
 
 export default {
   name: 'FeAddButton',
-
   props: {
     title: {
       type: String,
       default: 'title',
     },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   },
   methods: {
     handleAddButton() {
-      this.$emit('click');
+      !this.disabled && this.$emit('click');
     },
   },
 
@@ -32,6 +37,14 @@ export default {
   display: inline-flex;
   align-items: center;
   cursor: pointer;
+  background-color: transparent;
+  border: 0px transparent;
+  outline: 0px transparent;
+
+  &--disabled {
+    cursor: not-allowed;
+    opacity: .5;
+  }
 
   &__icon {
     display: inline-block;
